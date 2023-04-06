697934c349cfad6eddded869fa1db90fe0661ff5
Alikhan <astanaproteam123@gmail.com>
2023-04-06 21:21:19 +0600

    creating function to find Fibonacci number

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(findFibonacci(n));
+    }
+
+    public static int findFibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return findFibonacci(n - 1) + findFibonacci(n - 2);
+        }
+    }
+}
